b26e437f35237c8ef0282c275509711edd90ba76
fogelmariya <fogelmariya@gmail.com>
2025-04-10 11:24:40 -0500

    Update src/main/java/pages/ShoppingBagPage.java

--- src/main/java/pages/ShoppingBagPage.java
+++ src/main/java/pages/ShoppingBagPage.java
@@ -12,7 +12,14 @@ public class ShoppingBagPage extends BasePage {
     @FindBy(css = "div.product-line-items-container")
     List<WebElement> itemsAddedToBagWidget;
 
+    @FindBy(css = "button.remove-item")
+    List<WebElement> removeItemButtons;
 
+    @FindBy(css = "div.cart-total")
+    WebElement cartTotal;
+
+    @FindBy(css = "div.empty-cart-message")
+    WebElement emptyCartMessage;
 
     public String itemAddedToBagWidgetGetText(){
         wait.until(ExpectedConditions.visibilityOfAllElements(itemsAddedToBagWidget));
@@ -20,5 +27,32 @@ public class ShoppingBagPage extends BasePage {
         return itemAddedToBagWidget.findElement(By.xpath(".//div[@class='line-item-name']")).getText();
     }
 
+    public void removeItemFromCart(String itemName) {
+        for (WebElement item : itemsAddedToBagWidget) {
+            if (item.findElement(By.xpath(".//div[@class='line-item-name']")).getText().equals(itemName)) {
+                item.findElement(By.xpath(".//button[@class='remove-item']")).click();
+                wait.until(ExpectedConditions.stalenessOf(item));
+                break;
+            }
+        }
+    }
 
+    public boolean isItemRemovedFromCart(String itemName) {
+        for (WebElement item : itemsAddedToBagWidget) {
+            if (item.findElement(By.xpath(".//div[@class='line-item-name']")).getText().equals(itemName)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public String getCartTotal() {
+        wait.until(ExpectedConditions.visibilityOf(cartTotal));
+        return cartTotal.getText();
+    }
+
+    public boolean isCartEmptyMessageDisplayed() {
+        wait.until(ExpectedConditions.visibilityOf(emptyCartMessage));
+        return emptyCartMessage.isDisplayed();
+    }
 }
