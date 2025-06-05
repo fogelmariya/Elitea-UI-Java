6c080ca4f2ab4de164fac8d2ea4b3957a75b4f4e
fogelmariya <fogelmariya@gmail.com>
2025-06-05 15:09:17 -0500

    Update src/main/java/pages/ShoppingBagPage.java

--- src/main/java/pages/ShoppingBagPage.java
+++ src/main/java/pages/ShoppingBagPage.java
@@ -12,7 +12,14 @@ public class ShoppingBagPage extends BasePage {
     @FindBy(css = "div.product-line-items-container")
     List<WebElement> itemsAddedToBagWidget;
 
+    @FindBy(css = ".grand-total")
+    WebElement cartTotalElement;
 
+    @FindBy(css = ".cart-empty")
+    WebElement emptyCartMessage;
+
+    @FindBy(css = ".remove-product")
+    List<WebElement> removeButtons;
 
     public String itemAddedToBagWidgetGetText(){
         wait.until(ExpectedConditions.visibilityOfAllElements(itemsAddedToBagWidget));
@@ -20,5 +27,36 @@ public class ShoppingBagPage extends BasePage {
         return itemAddedToBagWidget.findElement(By.xpath(".//div[@class='line-item-name']")).getText();
     }
 
+    public boolean isItemInCart(String itemName) {
+        return itemsAddedToBagWidget.stream()
+                .anyMatch(item -> item.findElement(By.xpath(".//div[@class='line-item-name']")).getText().contains(itemName));
+    }
 
+    public void removeItem(String itemName) {
+        WebElement itemToRemove = itemsAddedToBagWidget.stream()
+                .filter(item -> item.findElement(By.xpath(".//div[@class='line-item-name']")).getText().contains(itemName))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Item not found in cart: " + itemName));
+
+        WebElement removeButton = itemToRemove.findElement(By.cssSelector(".remove-product"));
+        removeButton.click();
+        wait.until(ExpectedConditions.stalenessOf(itemToRemove));
+    }
+
+    public String getCartTotal() {
+        return cartTotalElement.getText();
+    }
+
+    public boolean isCartEmpty() {
+        return itemsAddedToBagWidget.isEmpty();
+    }
+
+    public String getEmptyCartMessage() {
+        return emptyCartMessage.getText();
+    }
+
+    public void open() {
+        driver.get(baseUrl + "/cart");
+        wait.until(ExpectedConditions.visibilityOf(cartTotalElement));
+    }
 }
