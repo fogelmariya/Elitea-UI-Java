54066c63be09698545f85658b5cc0facda895c73
fogelmariya <fogelmariya@gmail.com>
2025-06-06 11:58:29 -0500

    Update src/main/java/pages/ShoppingBagPage.java

--- src/main/java/pages/ShoppingBagPage.java
+++ src/main/java/pages/ShoppingBagPage.java
@@ -12,7 +12,14 @@ public class ShoppingBagPage extends BasePage {
     @FindBy(css = "div.product-line-items-container")
     List<WebElement> itemsAddedToBagWidget;
 
+    @FindBy(css = ".remove-product")
+    private WebElement removeItemButton;
 
+    @FindBy(css = ".grand-total-sum")
+    private WebElement cartTotalElement;
+
+    @FindBy(css = ".cart-empty")
+    private WebElement emptyCartMessage;
 
     public String itemAddedToBagWidgetGetText(){
         wait.until(ExpectedConditions.visibilityOfAllElements(itemsAddedToBagWidget));
@@ -20,5 +27,26 @@ public class ShoppingBagPage extends BasePage {
         return itemAddedToBagWidget.findElement(By.xpath(".//div[@class='line-item-name']")).getText();
     }
 
+    public boolean isItemInCart(String itemName) {
+        return itemAddedToBagWidgetGetText().contains(itemName);
+    }
 
+    public void removeItem() {
+        wait.until(ExpectedConditions.elementToBeClickable(removeItemButton));
+        removeItemButton.click();
+    }
+
+    public String getCartTotal() {
+        wait.until(ExpectedConditions.visibilityOf(cartTotalElement));
+        return cartTotalElement.getText();
+    }
+
+    public boolean isCartEmpty() {
+        return itemsAddedToBagWidget.isEmpty();
+    }
+
+    public String getEmptyCartMessage() {
+        wait.until(ExpectedConditions.visibilityOf(emptyCartMessage));
+        return emptyCartMessage.getText();
+    }
 }
