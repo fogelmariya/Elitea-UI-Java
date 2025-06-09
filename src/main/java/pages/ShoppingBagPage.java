ff3cdd5f3c6b71817e81ddda690d786ca48449d3
fogelmariya <fogelmariya@gmail.com>
2025-06-09 12:18:41 -0500

    Update src/main/java/pages/ShoppingBagPage.java

--- src/main/java/pages/ShoppingBagPage.java
+++ src/main/java/pages/ShoppingBagPage.java
@@ -12,7 +12,15 @@ public class ShoppingBagPage extends BasePage {
     @FindBy(css = "div.product-line-items-container")
     List<WebElement> itemsAddedToBagWidget;
 
+    @FindBy(css = ".grand-total")
+    WebElement cartTotalElement;
 
+    @FindBy(css = ".cart-empty")
+    WebElement emptyCartMessage;
+
+    public void open() {
+        driver.get(baseUrl + "/cart");
+    }
 
     public String itemAddedToBagWidgetGetText(){
         wait.until(ExpectedConditions.visibilityOfAllElements(itemsAddedToBagWidget));
@@ -20,5 +28,27 @@ public class ShoppingBagPage extends BasePage {
         return itemAddedToBagWidget.findElement(By.xpath(".//div[@class='line-item-name']")).getText();
     }
 
+    public boolean isItemInCart(String itemName) {
+        return itemsAddedToBagWidget.stream()
+                .anyMatch(item -> item.findElement(By.xpath(".//div[@class='line-item-name']")).getText().contains(itemName));
+    }
 
+    public void removeItem(String itemName) {
+        WebElement itemToRemove = itemsAddedToBagWidget.stream()
+                .filter(item -> item.findElement(By.xpath(".//div[@class='line-item-name']")).getText().contains(itemName))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Item not found in cart: " + itemName));
+
+        WebElement removeButton = itemToRemove.findElement(By.cssSelector(".remove-product"));
+        removeButton.click();
+        wait.until(ExpectedConditions.stalenessOf(itemToRemove));
+    }
+
+    public String getCartTotal() {
+        return cartTotalElement.getText().replaceAll("[^0-9.]", "");
+    }
+
+    public boolean isEmptyCartMessageDisplayed(String expectedMessage) {
+        return wait.until(ExpectedConditions.visibilityOf(emptyCartMessage)).getText().equals(expectedMessage);
+    }
 }
