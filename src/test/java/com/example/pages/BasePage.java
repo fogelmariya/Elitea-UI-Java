aed06b0057c1543e9f2a1f5e11b0e5f1c0f8ce83
fogelmariya <fogelmariya@gmail.com>
2025-08-14 13:02:58 -0500

    Create src/test/java/com/example/pages/BasePage.java

---
+++ src/test/java/com/example/pages/BasePage.java
@@ -0,0 +1,14 @@
+package com.example.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BasePage {
+    protected static WebDriver driver;
+    protected static WebDriverWait wait;
+
+    public static void setDriver(WebDriver webDriver) {
+        driver = webDriver;
+        wait = new WebDriverWait(driver, 10);
+    }
+}
