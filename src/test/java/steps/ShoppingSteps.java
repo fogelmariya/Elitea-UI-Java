45ae2e0aa96669e13a7ace8f0d7983a4b7da70af
fogelmariya <fogelmariya@gmail.com>
2025-08-14 13:16:06 -0500

    Update src/test/java/steps/ShoppingSteps.java

--- src/test/java/steps/ShoppingSteps.java
+++ src/test/java/steps/ShoppingSteps.java
@@ -1,17 +1,22 @@
 package steps;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
 import org.openqa.selenium.WebDriver;
 import pages.ItemDetailsPage;
 import pages.MainPage;
 import pages.ShoppingPage;
 import pages.SignInPage;
-
+import pages.ShoppingBagPage;
 
 public class ShoppingSteps {
 
     protected WebDriver driver;
     MainPage mainPage = new MainPage();
     ShoppingPage shoppingPage = new ShoppingPage();
+    ItemDetailsPage itemDetailsPage = new ItemDetailsPage();
+    ShoppingBagPage shoppingBagPage = new ShoppingBagPage();
 
     public void SignIn(String userEmail, String userPassword)
     {
@@ -32,7 +37,6 @@ public class ShoppingSteps {
 
     public void addItemToBag(){
         shoppingPage.clickOnFirstItem();
-        ItemDetailsPage itemDetailsPage = new ItemDetailsPage();
         itemDetailsPage.clickOnFirstSizeButton();
         itemDetailsPage.clickAddToBagButton();
     }
@@ -44,5 +48,38 @@ public class ShoppingSteps {
 
     public String getCartSuccessWidgetText(){
         return shoppingPage.getShoppingBagWidgetText();
+    }
+
+    @Given("I am on a product page")
+    public void iAmOnAProductPage() {
+        mainPage.open();
+        searchByItem("example product");
+        shoppingPage.clickOnFirstItem();
+    }
+
+    @When("I click the {string} button")
+    public void iClickTheButton(String buttonName) {
+        if (buttonName.equals("Add to Cart")) {
+            itemDetailsPage.clickOnFirstSizeButton();
+            itemDetailsPage.clickAddToBagButton();
+        } else {
+            throw new IllegalArgumentException("Unsupported button: " + buttonName);
+        }
+    }
+
+    @Then("the item should be added to my cart")
+    public void theItemShouldBeAddedToMyCart() {
+        viewShoppingCart();
+        assert shoppingBagPage.hasItems();
+    }
+
+    @Then("I should see a confirmation message")
+    public void iShouldSeeAConfirmationMessage() {
+        assert !getCartSuccessWidgetText().isEmpty();
+    }
+
+    @Then("the cart icon should update to reflect the new item count")
+    public void theCartIconShouldUpdateToReflectTheNewItemCount() {
+        assert shoppingPage.getCartItemCount() > 0;
     }
 }
