17ddd15010b9040992a779c7521ac8093abfff7c
fogelmariya <fogelmariya@gmail.com>
2025-08-14 13:11:01 -0500

    Update src/test/java/steps/ShoppingSteps.java

--- src/test/java/steps/ShoppingSteps.java
+++ src/test/java/steps/ShoppingSteps.java
@@ -1,17 +1,53 @@
 package steps;
 
+import io.cucumber.java.en.*;
 import org.openqa.selenium.WebDriver;
 import pages.ItemDetailsPage;
 import pages.MainPage;
 import pages.ShoppingPage;
 import pages.SignInPage;
-
+import org.junit.Assert;
 
 public class ShoppingSteps {
 
     protected WebDriver driver;
     MainPage mainPage = new MainPage();
     ShoppingPage shoppingPage = new ShoppingPage();
+    ItemDetailsPage itemDetailsPage = new ItemDetailsPage();
+
+    @Given("the user is on the main page")
+    public void theUserIsOnTheMainPage() {
+        // Assuming the browser is already initialized and navigated to the main page
+        Assert.assertTrue("User is not on the main page", mainPage.isOnMainPage());
+    }
+
+    @When("the user searches for {string}")
+    public void theUserSearchesFor(String searchTerm) {
+        searchByItem(searchTerm);
+    }
+
+    @And("selects the first item from the search results")
+    public void selectsTheFirstItemFromTheSearchResults() {
+        shoppingPage.clickOnFirstItem();
+    }
+
+    @And("clicks the Add to Bag button")
+    public void clicksTheAddToBagButton() {
+        itemDetailsPage.clickOnFirstSizeButton();
+        itemDetailsPage.clickAddToBagButton();
+    }
+
+    @Then("the item should be added to the shopping bag")
+    public void theItemShouldBeAddedToTheShoppingBag() {
+        String widgetText = getCartSuccessWidgetText();
+        Assert.assertTrue("Item was not added to the bag", widgetText.contains("Item added to your bag"));
+    }
+
+    @And("the shopping bag should display the correct item count")
+    public void theShoppingBagShouldDisplayTheCorrectItemCount() {
+        int itemCount = shoppingPage.getShoppingBagItemCount();
+        Assert.assertTrue("Shopping bag does not contain the correct item count", itemCount > 0);
+    }
 
     public void SignIn(String userEmail, String userPassword)
     {
@@ -32,7 +68,6 @@ public class ShoppingSteps {
 
     public void addItemToBag(){
         shoppingPage.clickOnFirstItem();
-        ItemDetailsPage itemDetailsPage = new ItemDetailsPage();
         itemDetailsPage.clickOnFirstSizeButton();
         itemDetailsPage.clickAddToBagButton();
     }
